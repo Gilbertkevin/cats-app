77712d2d2a90d7092a6d2b555f695203beffa4ee
Kevin Gilbert <kgilbert.og@gmail.com>
2021-11-17 20:14:12 -0600

    initial commit with index action

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,16 @@
+class KittensController < ApplicationController
+  def index
+    kittens = Kitten.all
+    render json: kittens.as_json
+  end
+
+  def create
+    kitten = Kitten.new(
+      name: params[:name],
+      age: params[:age],
+      breed: params[:breed],
+    )
+    kitten.save
+    render json: kitten.as_json
+  end
+end
